081d7baf012bfc3df040708e15c1221d11031c9b
Jeff Lewis <jeffylew77@gmail.com>
2018-07-17 13:14:29 -0700

    [Dev] Adding more types and resolvers

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -5,34 +5,58 @@ const fetch = require('node-fetch');
 // import APIKEY from '/Users/jefflewis/Documents/Computer-Programming/Projects/Converted GraphQL REST APIs/open311-rest-api-to-graphql/config/config.js'
 
 
+// Date
+let today = new Date();
+
+
 // GraphQL: Resolvers
 const RESOLVERS = {
   Query: {
-    getBaltimoreServices: async (parent, args) => {
+    getBaltimoreServiceList: async (parent, args) => {
       const response = await
       fetch(`http://311.baltimorecity.gov/open311/v2/services.json`);
       return response.json();
     },
-    getChicagoServices: async (parent, args) => {
+    getBaltimoreServiceDefinitionByServiceCode: async (parent, args) => {
       const response = await
-      fetch(`http://311api.cityofchicago.org/open311/v2/services.json`);
+      fetch(`https://api.city.gov/dev/v2/services/${args.serviceCode}.json`);
       return response.json();
     },
-    getBloomingtonServices: async (parent, args) => {
+    getAllBaltimoreOpenServiceRequests: async (parent, args) => {
       const response = await
-      fetch(`https://bloomington.in.gov/crm/open311/v2/services.json`);
+      fetch(`https://api.city.gov/dev/v2/requests.json?start_date=2000-01-01T00:00:00Z&end_date=${today}&status=open`);
       return response.json();
     },
-    getBostonServices: async (parent, args) => {
+    getAllBaltimoreClosedServiceRequests: async (parent, args) => {
       const response = await
-      fetch(`https://mayors24.cityofboston.gov/open311/v2/services.json`);
+      fetch(`https://api.city.gov/dev/v2/requests.json?start_date=2000-01-01T00:00:00Z&end_date=${today}&status=closed`);
       return response.json();
-    },
-    getBrooklineServices: async (parent, args) => {
-      const response = await
-      fetch(`http://spot.brooklinema.gov/open311/v2/services.json`);
-      return response.json();
-    },
+    }
+
+
+
+
+
+    // getChicagoServices: async (parent, args) => {
+    //   const response = await
+    //   fetch(`http://311api.cityofchicago.org/open311/v2/services.json`);
+    //   return response.json();
+    // },
+    // getBloomingtonServices: async (parent, args) => {
+    //   const response = await
+    //   fetch(`https://bloomington.in.gov/crm/open311/v2/services.json`);
+    //   return response.json();
+    // },
+    // getBostonServices: async (parent, args) => {
+    //   const response = await
+    //   fetch(`https://mayors24.cityofboston.gov/open311/v2/services.json`);
+    //   return response.json();
+    // },
+    // getBrooklineServices: async (parent, args) => {
+    //   const response = await
+    //   fetch(`http://spot.brooklinema.gov/open311/v2/services.json`);
+    //   return response.json();
+    // },
     // getColumbusServices: async (parent, args) => {
     //   const response = await
     //   fetch(``);
@@ -43,16 +67,16 @@ const RESOLVERS = {
     //   fetch(``);
     //   return response.json();
     // },
-    getPeoriaServices: async (parent, args) => {
-      const response = await
-      fetch(`https://ureport.peoriagov.org/crm/open311/v2/services.json`);
-      return response.json();
-    },
-    getSanFranciscoServices: async (parent, args) => {
-      const response = await
-      fetch(`http://mobile311.sfgov.org/open311/v2/services.json`);
-      return response.json();
-    },
+    // getPeoriaServices: async (parent, args) => {
+    //   const response = await
+    //   fetch(`https://ureport.peoriagov.org/crm/open311/v2/services.json`);
+    //   return response.json();
+    // },
+    // getSanFranciscoServices: async (parent, args) => {
+    //   const response = await
+    //   fetch(`http://mobile311.sfgov.org/open311/v2/services.json`);
+    //   return response.json();
+    // },
     // getWashingtonDCServices: async (parent, args) => {
     //   const response = await
     //   fetch(``);
--- server/graphql/types.js
+++ server/graphql/types.js
@@ -1,18 +1,13 @@
 // GraphQL: TypeDefs
 const TYPEDEFS = `
   type Query {
-    getBaltimoreServices: [Service]
-    getChicagoServices: [Service]
-    getBloomingtonServices: [Service]
-    getBostonServices: [Service]
-    getBrooklineServices: [Service]
-    getGrandRapidsServices: [Service]
-    getPeoriaServices: [Service]
-    getSanFranciscoServices: [Service]
-    getWashingtonDCServices: [Service]
+    getBaltimoreServiceList: [ServiceList]
+    getBaltimoreServiceDefinitionByServiceCode (code: String): [ServiceDefinition]
+    getAllBaltimoreOpenServiceRequests: [ServiceRequest]
+    getAllBaltimoreClosedServiceRequests: [ServiceRequest]
   }
 
-  type Service {
+  type ServiceList {
     service_code: String
     service_name: String
     description: String
@@ -34,6 +29,48 @@ const TYPEDEFS = `
     key: String
     name: String
   }
+
+  type ServiceRequest {
+    service_request_id: String
+    status: String
+    status_notes: String
+    service_name: String
+    service_code: String
+    description: String
+    agency_responsible: String
+    service_notice: String
+    requested_datetime: String
+    updated_datetime: String
+    expected_datetime: String
+    address: String
+    address_id: String
+    zipcode: String
+    lat: String
+    long: String
+    media_url: String
+  }
+
+  type Service {
+    service_code: String
+    attributes: [Attribute]
+  }
+
+  type Attribute {
+    variable: Boolean
+    code: String
+    datatype: String
+    required: Boolean
+    datatype_description: String
+    order: Int
+    description: String
+    values: [Value]
+  }
+
+  type Value {
+    key: Int
+    name: String
+  }
+
 `;
 
 
